f36ea6cb581c47318163e9c37065c86f2c4e8479
Samuel Lee <lsme8821@gmail.com>
2025-05-29 00:54:56 +0900

    feat: Install scripts and rootfs for Buildroot

---
+++ install-to-rootfs-buildroot.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+ROOTFS_PATH=$1
+if [ -z "$ROOTFS_PATH" ]; then
+    echo "Usage: $0 <path-to-rootfs>"
+    exit 1
+fi
+
+rsync -av --exclude='.git' --exclude='__pycache__' --exclude='*.pyc' ./rootfs/buildroot/ "$ROOTFS_PATH/"
+rsync -av --exclude='.git' --exclude='__pycache__' --exclude='*.pyc' --exclude='rootfs' ./ "$ROOTFS_PATH/root/PS4JB"
+
+# Configure USB device
+sed -i '/^UMS_EN=/s/off$/on/' "$ROOTFS_PATH/etc/init.d/S50usbdevice"
+
+echo "Repackaging firmware required with new rootfs. Run \`./build.sh firmware\` on luckfox-pico SDK directory"
